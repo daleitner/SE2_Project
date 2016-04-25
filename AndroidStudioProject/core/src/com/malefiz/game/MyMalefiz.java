b7d925538a7248a4e1cc04d16e38642bcc06fa3f
leusl <dleustik@hotmail.com>
2016-04-25 21:05:56 +0200

    rudimental startscreen

--- AndroidStudioProject/core/src/com/malefiz/game/MyMalefiz.java
+++ AndroidStudioProject/core/src/com/malefiz/game/MyMalefiz.java
@@ -2,26 +2,43 @@ package com.malefiz.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.malefiz.game.screens.CharacterScreen;
+import com.malefiz.game.screens.MenuScreen;
+import com.badlogic.gdx.Game;
 
-public class MyMalefiz extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class MyMalefiz extends Game {
+	MenuScreen ms;
+	CharacterScreen cs;
+	Screen sc;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setMenuScreen();
+	}
+
+	public void setMenuScreen() {
+		ms = new MenuScreen(this);
+		sc = ms;
+		ms.show();
+		setScreen(ms);
+	}
+
+	public void setCharacterScreen() {
+		cs = new CharacterScreen(this);
+		sc.hide();
+		sc = cs;
+		cs.show();
+		setScreen(cs);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		sc.render(0.0001f);
 	}
 }
