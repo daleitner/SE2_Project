0d59c7beb42888e4364d9738552dac34f41a5b92
Daniel Leitner <dmcleite@hotmail.com>
2016-05-21 11:29:29 +0200

    bug fix. CharacterSelectionController ctor aufruf um Mode erweitert

--- AndroidStudioProject/core/src/junittests/CharacterSelectionTests.java
+++ AndroidStudioProject/core/src/junittests/CharacterSelectionTests.java
@@ -1,5 +1,6 @@
 
 import controllers.CharacterSelectionController;
+import models.Mode;
 
 
 import org.junit.Test;
@@ -15,39 +16,39 @@ public class CharacterSelectionTests {
 
     @Test
     public void AfterInitializingFourCharactersAreCreated() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         assertEquals(4, sut.getCharacters().size());
     }
     @Test
     public void AfterInitializingNoCharacterIsSelected() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         assertNull(sut.getSelectedCharacter());
     }
 
     @Test
     public void WhenClickingFirstCharacter_SelectFirstCharacter() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         sut.selectSingleCharacter(0);
         assertEquals(sut.getCharacters().get(0), sut.getSelectedCharacter());
     }
 
     @Test
     public void WhenClickingSecondCharacter_SelectSecondCharacter() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         sut.selectSingleCharacter(1);
         assertEquals(sut.getCharacters().get(1), sut.getSelectedCharacter());
     }
 
     @Test
     public void WhenClickingThirdCharacter_SelectThirdCharacter() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         sut.selectSingleCharacter(2);
         assertEquals(sut.getCharacters().get(2), sut.getSelectedCharacter());
     }
 
     @Test
     public void WhenClickingFourthCharacter_SelectFourthCharacter() {
-        CharacterSelectionController sut = new CharacterSelectionController(null, null);
+        CharacterSelectionController sut = new CharacterSelectionController(null, null, Mode.LOCAL);
         sut.selectSingleCharacter(3);
         assertEquals(sut.getCharacters().get(3), sut.getSelectedCharacter());
     }
